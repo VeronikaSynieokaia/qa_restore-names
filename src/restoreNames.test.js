3e6e9ba679039fc2a69b2f1c4b5842039b7819b8
Вероника Синеокая <veronikasineokaa@Veronikas-MacBook-Pro.local>
2023-03-01 11:22:44 +0100

    Tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,94 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it(`shouldn't set firstName if it is existing`, () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
 
+    restoreNames(users);
+
+    const expected = [
+      {
+        firstName: 'John',
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    expect(users).toEqual(expected);
   });
 
-  // write tests here
+  it(`should set firstName if it is not present`, () => {
+    const users = [
+      {
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    restoreNames(users);
+
+    const expected = [
+      {
+        firstName: 'John',
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    expect(users)
+      .toEqual(expected);
+  });
+
+  it(`should set lastName if it is not present`, () => {
+    const users = [
+      {
+        firstName: 'John',
+        fullName: 'John Snow',
+      },
+    ];
+
+    restoreNames(users);
+
+    const expected = [
+      {
+        firstName: 'John',
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    expect(users)
+      .toEqual(expected);
+  });
+
+  it(`should set firstName if it is null`, () => {
+    const users = [
+      {
+        firstName: null,
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    restoreNames(users);
+
+    const expected = [
+      {
+        firstName: 'John',
+        lastName: 'Snow',
+        fullName: 'John Snow',
+      },
+    ];
+
+    expect(users)
+      .toEqual(expected);
+  });
 });
